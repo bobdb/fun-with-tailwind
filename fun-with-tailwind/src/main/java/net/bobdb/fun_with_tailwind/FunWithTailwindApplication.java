2a6517a786a799456c067f90f432d5f11bde30d8
bobdb <bobdb40@gmail.com>
2024-08-12 22:10:25 -0400

    init

---
+++ fun-with-tailwind/src/main/java/net/bobdb/fun_with_tailwind/FunWithTailwindApplication.java
@@ -0,0 +1,13 @@
+package net.bobdb.fun_with_tailwind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunWithTailwindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunWithTailwindApplication.class, args);
+	}
+
+}
