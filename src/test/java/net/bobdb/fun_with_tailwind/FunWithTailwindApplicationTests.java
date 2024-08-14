578b690eb04b8bebc24a8e13608428dd8b7c3763
bobdb <bobdb40@gmail.com>
2024-08-13 21:45:14 -0400

    init

---
+++ src/test/java/net/bobdb/fun_with_tailwind/FunWithTailwindApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bobdb.fun_with_tailwind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunWithTailwindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
